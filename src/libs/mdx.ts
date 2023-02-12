7efdef6cb41dcd0bbc998e14d7e38a3d4687e805
ywbird <ywbird@naver.com>
2023-02-12 14:34:11 +0900

    set font to NeoDunggeunmo, Galmuri

--- src/libs/mdx.ts
+++ src/libs/mdx.ts
@@ -7,14 +7,42 @@ import remarkBreaks from 'remark-breaks';
 import remarkGfm from 'remark-gfm';
 import rehypeCodeTitles from 'rehype-code-titles';
 
+import rehypePrettyCode from 'rehype-pretty-code';
+
+const options = {
+  // Use one of Shiki's packaged themes
+  theme: 'material-theme-ocean',
+  // Keep the background or use a custom background color?
+  keepBackground: true,
+
+  // Callback hooks to add custom logic to nodes when visiting
+  // them.
+  onVisitLine(node: any) {
+    // Prevent lines from collapsing in `display: grid` mode, and
+    // allow empty lines to be copy/pasted
+    if (node.children.length === 0) {
+      node.children = [{ type: 'text', value: ' ' }];
+    }
+  },
+  onVisitHighlightedLine(node: any) {
+    // Each line node by default has `class="line"`.
+    node.properties.className.push('highlighted');
+  },
+  onVisitHighlightedWord(node: any) {
+    // Each word node has no className by default.
+    node.properties.className = ['word'];
+  },
+};
+
 export const serializeMdx = (source: string) => {
   return serialize(source, {
     mdxOptions: {
       remarkPlugins: [remarkGfm, remarkBreaks],
       rehypePlugins: [
         rehypeSlug,
-        rehypeCodeTitles,
-        rehypePrism,
+        // rehypeCodeTitles,
+        // rehypePrism,
+        [rehypePrettyCode, options],
         [
           rehypeAutolinkHeadings,
           {

6c691eae0cad06e81a4fc905126b6c051fe92abe
ywbird <ywbird@naver.com>
2023-02-12 15:20:06 +0900

    font self host

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,8 @@ import type { AppProps } from 'next/app';
 // import '@/styles/prism-one-dark.css';
 import '@/styles/globals.css';
 
+import 'galmuri/dist/galmuri.css';
+
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }

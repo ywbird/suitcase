73fad6518d42e288fcab76d78ccc6683cba485f7
ywbird <ywbird@naver.com>
2023-02-12 10:53:24 +0900

    base path /suitcase on production

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/suitcase',
 };
+
+if (process.env.NODE_ENV === 'production') {
+  nextConfig.basePath = '/suitcase';
+}
 
 module.exports = nextConfig;

fba0979339500231ffe5179855b009952c705a2b
ywbird <ywbird@naver.com>
2023-02-12 16:31:56 +0900

    make my own neodgm-pro package and add it

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,9 @@ import type { AppProps } from 'next/app';
 
 // import '@/styles/prism-one-dark.css';
 import '@/styles/globals.css';
-import '/fonts/neodgm_pro/style.css';
 
 import 'galmuri/dist/galmuri.css';
+import 'neodgm-pro';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;

f8cb7052e2f097b037b0b23812a1058f956c5beb
ywbird <ywbird@naver.com>
2023-02-12 15:34:09 +0900

    font basepath 적용 안됨

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app';
 
 // import '@/styles/prism-one-dark.css';
 import '@/styles/globals.css';
+import '/fonts/neodgm_pro/style.css';
 
 import 'galmuri/dist/galmuri.css';
 
